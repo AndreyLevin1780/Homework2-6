b9eaa95d9133d54702e66d91d264159d7cbadb63
AndreyLevin1780 <andreylevin1980@mail.ru>
2024-05-08 17:30:54 +0300

    work branch

--- src/main/java/pro/sky/EmployeesCollectionHomework/EmployeeService.java
+++ src/main/java/pro/sky/EmployeesCollectionHomework/EmployeeService.java
@@ -5,46 +5,48 @@ import pro.sky.EmployeesCollectionHomework.Exceptions.EmployeeAlreadyAddedExcept
 import pro.sky.EmployeesCollectionHomework.Exceptions.EmployeeNotFoundException;
 import pro.sky.EmployeesCollectionHomework.Exceptions.EmployeeStorageIsFullException;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class EmployeeService {
 
     private final int maxNumberOfEmployees = 10;
-    List<Employee> employees = new ArrayList<>();
+    private final Map<String, Employee> employees = new HashMap();
 
     public Employee add(String firstName, String lastName) {
+        String key = buildKey(firstName, lastName);
         Employee employee = new Employee(firstName, lastName);
-        if (employees.contains(employee)) {
+        if (employees.containsKey(key)) {
             throw new EmployeeAlreadyAddedException();
         }
         if (employees.size() >= maxNumberOfEmployees) {
             throw new EmployeeStorageIsFullException();
         }
-        employees.add(employee);
+        employees.put(key,employee);
         return employee;
     }
 
     public Employee remove(String firstName, String lastName) {
-        Employee employee = new Employee(firstName, lastName);
-        if (!employees.contains(employee)) {
+        String key = buildKey(firstName, lastName);
+        if (!employees.containsKey(key)) {
             throw new EmployeeNotFoundException();
         }
-        employees.remove(employee);
-        return employee;
-
+        return employees.remove(key);
     }
+
     public Employee find(String firstName, String lastName) {
-        Employee employee = new Employee(firstName, lastName);
-        if (!employees.contains(employee)) {
+        String key = buildKey(firstName, lastName);
+        if (!employees.containsKey(key)) {
             throw new EmployeeNotFoundException();
         }
-        return employee;
+        return employees.get(key);
+    }
+
+    public String buildKey (String name, String surename) {
+        return name + surename;
     }
 
     public List<Employee> findAll() {
-        return Collections.unmodifiableList(employees);
+        return List.copyOf(employees.values());
     }
 }
